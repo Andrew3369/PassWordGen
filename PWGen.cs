794fce96a1341548afb5ab395cf37b8cc425b60f
Andrew3369 <andrewjanosbabos@gmail.com>
2023-10-27 01:41:07 -0400

    Created a password generator, all randomly generated texts are effectively unique

---
+++ PWGen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RandomPWGen
+{
+    internal class PWGen
+    {
+        private readonly int NumOfPWs;
+
+        // METHOD: ThreadsTasks
+        // PURPOSE: Instantiate the Threads and Tasks class with the cmd arguments
+        // RETURNS: NONE
+        public PWGen(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                NumOfPWs = 1;
+            }
+            else
+            {
+                NumOfPWs = int.Parse(args[0]);
+            }
+        }
+
+        // METHOD: GeneratePWs
+        // PURPOSE: Generates the new passwords to the CMDline
+        // RETURNS: NONE
+        public void GeneratePWs()
+        {
+            Console.WriteLine("Potential Passwords: \n");
+            for (int i = 0; i < NumOfPWs; i++)
+            {
+                Guid randomPassword = Guid.NewGuid();
+                Console.WriteLine("\tPassword #{0}: {1}", i, randomPassword.ToString());
+            }
+        }
+    }
+}
---
+++ PassGen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RandomPWGen;
+
+namespace RandomPWGen
+{
+    internal class PassGen
+    {
+        static void Main(string[] args)
+        {
+            PWGen pWGen = new PWGen(args);
+            pWGen.GeneratePWs();
+        }
+    }
+}
