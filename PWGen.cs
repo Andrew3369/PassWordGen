998cafaccc9b2fe3fdc4293dc94bbd1c439d1161
Andrew3369 <andrewjanosbabos@gmail.com>
2023-10-27 02:36:59 -0400

    Added in another method that handles displaying the menu as well as navigation for the user (Leaves the main function in PassGen.cs more clean). Fixed another bug that didnt copy the correct GUID from the displayed GUID

--- PWGen.cs
+++ PWGen.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿//File:             PWGen.cs
+//Programmer:       Andrew Babos
+//First Version:    10-27-2023
+
+using System;
+using System.Windows;
+using System.Threading;
 
 namespace RandomPWGen
 {
     internal class PWGen
     {
-        private readonly int NumOfPWs;
         private readonly string characters;
 
         // METHOD: PWGen
@@ -12,15 +17,106 @@ namespace RandomPWGen
         // RETURNS: NONE
         public PWGen(string[] args)
         {
-            if (args.Length == 0)
-            {
-                NumOfPWs = 1;
-            }
-            else
-            {
-                NumOfPWs = int.Parse(args[0]);
-            }
             characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-{}[]|:;<>?,./";
+        }
+
+        //METHOD: PWGenMenu
+        //PURPOSE: Displays the Menu Navigation of the Program
+        //RETURNS: None (void)
+        public void PWGenMenu()
+        {
+            while (true)
+            {
+                // Menu Selection
+                Console.WriteLine
+                (
+                "1. Create a Unique Password using GUID Generator (36 Letters in Length, Very Secure)\n" +
+                "2. Create a Unique Password using a already set Character determination (16 Letters in Length, Less Secure than GUID)\n" +
+                "3. Exit\n"
+                );
+
+                // User selection
+                string response = Console.ReadLine();
+
+                // Password Generator from GUID
+                if (response == "1")
+                {
+                    // Set password
+                    string password = GeneratePWs();
+                    Console.WriteLine("\tPassword: {0}\n", password);
+                    Console.WriteLine("\tWould you like to copy the password to the clipboard?\n");
+
+                    response = Console.ReadLine();
+
+                    // If input is yes, copy to clipboard
+                    if (response == "Y" || response == "y" || response == "yes" || response == "Yes")
+                    {
+                        Thread thread = new Thread(() => Clipboard.SetText(password));
+                        Console.WriteLine("\tPassword Copied to Clipboard!\n");
+                        thread.SetApartmentState(ApartmentState.STA);
+                        thread.Start();
+                        thread.Join();
+                    }
+
+                    // "No" input, do not copy to clipboard
+                    else if (response == "N" || response == "n" || response == "No" || response == "no")
+                    {
+                        Console.WriteLine("\tPassword Not Copied to Clipboard!\n");
+                    }
+
+                    // Invalid Options recieved
+                    else
+                    {
+                        Console.WriteLine("\tInvalid Response!\n");
+                    }
+                }
+
+                // Password Generator from Given Set of Characters
+                else if (response == "2")
+                {
+                    // Set password
+                    string password = GeneratePWs();
+                    Console.WriteLine("\tPassword: {0}\n", password);
+                    Console.WriteLine("\tWould you like to copy the password to the clipboard?\n");
+                    response = Console.ReadLine();
+
+                    // If input is yes, copy to clipboard
+                    if (response == "Y" || response == "y" || response == "yes" || response == "Yes")
+                    {
+                        Thread thread = new Thread(() => Clipboard.SetText(password));
+                        //Clipboard.SetText(pWGen.GeneratePWs());
+                        Console.WriteLine("\tPassword Copied to Clipboard!\n");
+                        thread.SetApartmentState(ApartmentState.STA);
+                        thread.Start();
+                        thread.Join();
+                    }
+
+                    // "No" input, do not copy to clipboard
+                    else if (response == "N" || response == "n" || response == "No" || response == "no")
+                    {
+                        Console.WriteLine("\tPassword Not Copied to Clipboard!\n");
+                    }
+
+                    // Invalid Options recieved
+                    else
+                    {
+                        Console.WriteLine("\tInvalid Response!\n");
+                    }
+                }
+
+                // Exit
+                else if (response == "3")
+                {
+                    Console.WriteLine("Exiting...");
+                    break;
+                }
+
+                // If 1 or 2 wasnt selected
+                else
+                {
+                    Console.WriteLine("Select One of the Options Above\nExiting...");
+                }
+            }
         }
 
         //METHOD: GeneratePWs
@@ -39,7 +135,7 @@ namespace RandomPWGen
 
         //METHOD: GeneratePWsGUID
         //PURPOSE: Generates the new passwords to the CMDline
-        //RETURNS: NONE
+        //RETURNS: string randomPassword
         public string GeneratePWsGUID()
         {
             Guid randomPassword = Guid.NewGuid();
--- PassGen.cs
+++ PassGen.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Windows;
-using System.Threading;
+﻿//File:             PassGen.cs
+//Programmer:       Andrew Babos
+//First Version:    10-27-2023
+
+using System;
 
 namespace RandomPWGen
 {
@@ -8,87 +10,11 @@ namespace RandomPWGen
     {
         static void Main(string[] args)
         {
-
             PWGen pWGen = new PWGen(args);
             Console.WriteLine("Welcome to Password Generator!\n");
 
-            while (true)
-            {
-                // Menu Selection
-                Console.WriteLine
-                (
-                "1. Create a Unique Password using GUID Generator (36 Letters in Length, Very Secure)\n" +
-                "2. Create a Unique Password using a already set Character determination (16 Letters in Length, Less Secure than GUID)\n"
-                );
-
-                // User selection
-                string response = Console.ReadLine();
-
-                // Password Generator from GUID
-                if (response == "1")
-                {
-                    Console.WriteLine("\tPassword: {0}\n", pWGen.GeneratePWsGUID());
-                    Console.WriteLine("\tWould you like to copy the password to the clipboard?\n");
-
-                    response = Console.ReadLine();
-
-                    if (response == "Y" || response == "y" || response == "yes" || response == "Yes")
-                    {
-                        Thread thread = new Thread(() => Clipboard.SetText(pWGen.GeneratePWsGUID()));
-                        Console.WriteLine("\tPassword Copied to Clipboard!\n");
-                        thread.SetApartmentState(ApartmentState.STA);
-                        thread.Start();
-                        thread.Join();
-                    }
-
-                    else if (response == "N" || response == "n" || response == "No" || response == "no")
-                    {
-                        Console.WriteLine("\tPassword Not Copied to Clipboard!\n");
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("\tInvalid Response!\n");
-                    }
-                }
-
-                // Password Generator from Given Set of Characters
-                else if (response == "2")
-                {
-                    //pWGen.GeneratePWs();
-                    string password = pWGen.GeneratePWs();
-                    Console.WriteLine("\tPassword: {0}\n", password);
-                    Console.WriteLine("\tWould you like to copy the password to the clipboard?\n");
-                    response = Console.ReadLine();
-
-                    if (response == "Y" || response == "y" || response == "yes" || response == "Yes")
-                    {
-                        Thread thread = new Thread(() => Clipboard.SetText(password));
-                        //Clipboard.SetText(pWGen.GeneratePWs());
-                        Console.WriteLine("\tPassword Copied to Clipboard!\n");
-                        thread.SetApartmentState(ApartmentState.STA);
-                        thread.Start();
-                        thread.Join();
-                    }
-
-                    else if (response == "N" || response == "n" || response == "No" || response == "no")
-                    {
-                        Console.WriteLine("\tPassword Not Copied to Clipboard!\n");
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("\tInvalid Response!\n");
-                    }
-                }
-
-                // If 1 or 2 wasnt selected
-                else
-                {
-                    Console.WriteLine("Select One of the Options Above\nExiting...");
-                    Environment.Exit(0);
-                }
-            }
+            // Start Program
+            pWGen.PWGenMenu();
         }
     }
 }
